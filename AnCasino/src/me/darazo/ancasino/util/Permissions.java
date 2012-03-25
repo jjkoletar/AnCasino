fccb05e0c4da6a5cf9dfba03fc5bfcec6e300b12
Jeremy Koletar <jjkoletar@gmail.com>
2012-03-24 20:02:53 -0500

    Cleanup permissions

--- AnCasino/src/me/darazo/ancasino/util/Permissions.java
+++ AnCasino/src/me/darazo/ancasino/util/Permissions.java
@@ -21,69 +21,40 @@ public class Permissions {
 	}
 
 	// Returns true if player has admin permissions
-	public Boolean isAdmin(Player player) {
-
-		if (player.isOp() || player.hasPermission(admin)) {
-			return true;
-		}
-		return false;
+	public boolean isAdmin(Player player) {
+		return player.isOp() || player.hasPermission(admin);
 	}
 
 	// Returns true if player can create slots
-	public Boolean canCreate(Player player) {
-
-		if (isAdmin(player) || player.hasPermission(create)) {
-			return true;
-		}
-		return false;
+	public boolean canCreate(Player player) {
+		return isAdmin(player) || player.hasPermission(create);
 	}
 
 	// Returns true if player can create slots with the type
-	public Boolean canCreate(Player player, Type type) {
-
-		String name = type.getName();
-		if (isAdmin(player) || player.hasPermission(create + "." + name)) {
-			return true;
-		}
-		return false;
+	public boolean canCreate(Player player, Type type) {
+		return isAdmin(player)
+				|| player.hasPermission(create + "." + type.getName());
 	}
 
 	// Returns true if player can create slots with the type
-	public Boolean canCreate(Player player, String type) {
-
-		if (isAdmin(player) || player.hasPermission(create + "." + type)) {
-			return true;
-		}
-		return false;
+	public boolean canCreate(Player player, String type) {
+		return isAdmin(player) || player.hasPermission(create + "." + type);
 	}
 
 	// Returns true if the player can manage slot machines
-	public Boolean canManage(Player player) {
-
-		if (isAdmin(player) || player.hasPermission(manage)) {
-			return true;
-		}
-		return false;
+	public boolean canManage(Player player) {
+		return isAdmin(player) || player.hasPermission(manage);
 	}
 
 	// Returns true if the player can use the type
-	public Boolean canUse(Player player, Type type) {
-
-		String name = type.getName();
-		if (isAdmin(player) || player.hasPermission(use + "." + name)) {
-			return true;
-		}
-		return false;
+	public boolean canUse(Player player, Type type) {
+		return isAdmin(player)
+				|| player.hasPermission(use + "." + type.getName());
 	}
 
-	public Boolean isOwner(Player player, SlotMachine slot) {
-
-		if (isAdmin(player)
+	public boolean isOwner(Player player, SlotMachine slot) {
+		return isAdmin(player)
 				|| (slot.getOwner() != null && slot.getOwner()
-						.equalsIgnoreCase(player.getName()))) {
-			return true;
-		}
-		return false;
+						.equalsIgnoreCase(player.getName()));
 	}
-
 }
